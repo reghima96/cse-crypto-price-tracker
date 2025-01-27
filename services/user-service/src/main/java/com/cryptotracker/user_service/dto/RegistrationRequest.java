faf7408362cedd7aae383e4f89cfbc5b300e885b
reghima96 <mure1001@stud.hs-kl.de>
2025-01-27 23:01:41 +0100

    hotfix :added missing Dockerfile

--- services/user-service/src/main/java/com/cryptotracker/user_service/dto/RegistrationRequest.java
+++ services/user-service/src/main/java/com/cryptotracker/user_service/dto/RegistrationRequest.java
@@ -1,11 +1,14 @@
 package com.cryptotracker.user_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class RegistrationRequest {
     @NotBlank(message = "Name is required")
     private String name;
@@ -14,34 +17,10 @@ public class RegistrationRequest {
     @Email(message = "Invalid email format")
     private String email;
 
+    @JsonIgnore
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters long.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", 
-         message = "Password must include letters, numbers, and special characters.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must include letters, numbers, and special characters.")
     private String password;
 
-    // Getters and setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
